79e46af1155f3acda2e858c08ab27e29089e7a74
nutfac <goolb3m@gmail.com>
2024-02-23 17:32:15 +0900

    타이머 카드 파일 생성

---
+++ lib/common/widget/w_timer_card.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TimerCard extends StatefulWidget {
+  const TimerCard({super.key});
+
+  @override
+  State<TimerCard> createState() => _TimerCardState();
+}
+
+class _TimerCardState extends State<TimerCard> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
